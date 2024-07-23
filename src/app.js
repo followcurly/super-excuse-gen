4897a0e6befe872e08ec91fd85e18a2992caacd7
followcurly <20099605+followcurly@users.noreply.github.com>
2024-07-22 20:45:22 -0400

    Initial commit of my super excuse generator to my own repo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,61 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+document.getElementById("ohshit").onclick = function() {
+  let who = [
+    "A ghost",
+    "My imaginary friend",
+    "A ninja",
+    "The alien",
+    "The time traveler",
+    "A talking cat",
+    "My evil twin",
+    "The invisible man"
+  ];
+  let action = [
+    "vaporized",
+    "teleported",
+    "enchanted",
+    "bewitched",
+    "exploded",
+    "shrunk",
+    "grew legs and ran away with",
+    "turned into glitter"
+  ];
+  let what = [
+    "my homework",
+    "my phone",
+    "the car",
+    "my keys",
+    "the remote",
+    "my wallet",
+    "my book",
+    "my glasses",
+    "my lunch",
+    "my shoes"
+  ];
+  let when = [
+    "before the class",
+    "when I was abducted by aliens",
+    "while I was time traveling",
+    "during the zombie apocalypse",
+    "while I was fighting ninjas",
+    "during my magic show",
+    "while I was invisible",
+    "when I was in another dimension"
+  ];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let random1 = Math.floor(Math.random() * who.length);
+  let random2 = Math.floor(Math.random() * action.length);
+  let random3 = Math.floor(Math.random() * what.length);
+  let random4 = Math.floor(Math.random() * when.length);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let excuse =
+    who[random1] +
+    " " +
+    action[random2] +
+    " " +
+    what[random3] +
+    " " +
+    when[random4];
+  let excuseElement = document.querySelector("#excuse");
+
+  excuseElement.innerHTML = excuse;
 };
